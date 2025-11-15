4c45aa7695d8c84638d419b03c98d4ace719d8b3
Jochen Klar <mail@jochenklar.de>
2025-11-15 10:15:06 +0100

    Update build_titiler_list.py

--- bin/build_titiler_list.py
+++ bin/build_titiler_list.py
@@ -1,9 +1,25 @@
+import argparse
+
 from gdal_tiles_api.config import load_config
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--include", nargs="*")
+parser.add_argument("--exclude", nargs="*")
+
+args = parser.parse_args()
 
 config = load_config()
 
 lines = []
 for map_settings in config.maps:
+    if args.include and not any(
+        str(map_settings.path).startswith(s) for s in args.include
+    ):
+        continue
+
+    if args.exclude and any(str(map_settings.path).startswith(s) for s in args.exclude):
+        continue
+
     lines.append(
         f"http://localhost:5000/cog/viewer?url={map_settings.get_dataset_path(config.tiles.path)}"
     )
